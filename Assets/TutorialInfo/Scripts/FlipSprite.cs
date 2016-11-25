dcffa3a0c983ede7feda23505c11e884b3bb72ac
Dave Tamayo <djbtamayo@gmail.com>
2016-11-24 19:12:54 -0500

    Last minute changes before presenting

---
+++ Assets/TutorialInfo/Scripts/FlipSprite.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipSprite : MonoBehaviour {
+
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+	
+		spriteRenderer = this.GetComponentInChildren<SpriteRenderer> ();
+		StartCoroutine (Flip ());
+	}
+	
+	IEnumerator Flip () {
+
+		yield return new WaitForSeconds(1);
+
+		spriteRenderer.flipX = !spriteRenderer.flipX;
+
+		StartCoroutine (Flip ());
+	}
+}
